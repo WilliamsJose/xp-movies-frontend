8f06cc53607c3ffdd84b5cb6d171fb76f615b402
will <contatowilliamsj@gmail.com>
2024-02-22 19:24:15 -0300

    start: next app with shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      Hello Next
+      <div>
+        <Button type="button">Shadcn</Button>
+      </div>
+    </main>
+  );
+}
